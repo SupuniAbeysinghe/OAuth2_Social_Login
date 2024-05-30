ba0b820ad05f28ff5ce19b0aacc4bc876c1fd32c
SupuniAbeysinghe <supuniab1@gmail.com>
2024-05-30 22:10:24 +0530

    implemented login with git hub account

---
+++ src/main/java/com/oauth2/OAuth/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.oauth2.OAuth.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login()
+
+                ;
+
+        return http.build();
+    }
+}
---
+++ src/test/java/com/oauth2/OAuth/OAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2.OAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
